87585b444e4046dbc25642b9cc7f9da593f016d7
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2023-12-08 01:39:24 -0500

    ytes

--- Day8/P2/Solution.java
+++ Day8/P2/Solution.java
@@ -6,59 +6,24 @@ public class Solution {
     public final static String fm = "final";
     public final static String INS = "LLRLRRLLRLRRLLRLRRLRRRLRLRLRRRLLRLRRRLRLRRRLRLRLLLRRLRLRLLRLRRLRRRLRRRLLRRLRLRRRLRRLRRRLRLLRRLRRRLRRRLRRLRLRRLLLRLRLLRRRLRRLLRLRLRRLLRLRRLLRLRRLRRLLRRRLRLRLRRRLLRRRLRRLRRRLRRRLRLRRRLRRLLLRRRLRLLLRRRLRLLRLLRRRLLRRLRRRLRRRLRLLRLRLRRRLLRRLRRRLRRLRLLRRRLRRLRRRLRRRLRRRLRLRRRLRRRLRLRRRR";
 
-    // public static String navigate(String current, String instruction, Map<String, String[]> nodes) {
-    //     String[] nextNodes = nodes.get(current);
-    //     return instruction.equals("R") ? nextNodes[1] : nextNodes[0];
-    // }
-
-    public static Set<String> navigate(Set<String> currentNodes, String instruction, Map<String, String[]> nodes) {
-        Set<String> nextNodes = new HashSet<>();
-        for (String current : currentNodes) {
-           String[] nextNodeArray = nodes.get(current);
-           String nextNode = instruction.equals("R") ? nextNodeArray[1] : nextNodeArray[0];
-           nextNodes.add(nextNode);
-        }
-        return nextNodes;
+    public static String navigate(String current, String instruction, Map<String, String[]> nodes) {
+        String[] nextNodes = nodes.get(current);
+        return instruction.equals("R") ? nextNodes[1] : nextNodes[0];
     }
 
-    // public static int countSteps(String instructions, Map<String, String[]> nodes) {
-    //     String current = START;
-    //     int steps = 0;
-    //     int instructionIndex = 0;
-    //     while (!current.equals(END)) {
-    //         String instruction = instructions.charAt(instructionIndex % instructions.length()) == 'R' ? "R" : "L";
-    //         current = navigate(current, instruction, nodes);
-    //         steps++;
-    //         instructionIndex++;
-    //         System.out.println(current + " " + (instructionIndex % instructions.length()));
-    //     }
-    //     return steps;
-    // }
-
-    public static int countSteps(Set<String> startNodes, String instructions, Map<String, String[]> nodes) {
-        Set<String> currentNodes = new HashSet<>(startNodes);
-        int steps = 0;
+    public static long countSteps(String start, String instructions, Map<String, String[]> nodes) {
+        String current = start;
+        long steps = 0;
         int instructionIndex = 0;
-        while (!allNodesEndWith(currentNodes, "Z")) {
+        while (!current.endsWith("Z")) {
             String instruction = instructions.charAt(instructionIndex % instructions.length()) == 'R' ? "R" : "L";
-            currentNodes = navigate(currentNodes, instruction, nodes);
+            current = navigate(current, instruction, nodes);
             steps++;
             instructionIndex++;
-            for (String s : currentNodes) {
-                System.out.println(s + " " + (instructionIndex % instructions.length()) + " " + steps);
-            }
+            System.out.println(current + " " + (instructionIndex % instructions.length()));
         }
         return steps;
-    }
-
-    public static boolean allNodesEndWith(Set<String> nodes, String suffix) {
-        for (String node : nodes) {
-           if (!node.endsWith(suffix)) {
-              return false;
-           }
-        }
-        return true;
-    }
+     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader r = new BufferedReader(new FileReader(fm + ".in"));
@@ -67,8 +32,21 @@ public class Solution {
 
         Map<String, String[]> nodes = new HashMap<>();
         List<String> lines = r.lines().collect(Collectors.toList());
-        Set<String> currentNodes = new HashSet<>();
-
+        // try (BufferedReader br = new BufferedReader(new FileReader(fm + ".in"))) {
+        //     String line;
+        //     while ((line = br.readLine()) != null) {
+        //         if (line.isEmpty()) break;
+        //         String[] parts = line.split(" = ");
+        //         String node = parts[0];
+        //         // String nextNodes = parts[1];
+        //         // System.out.println(nextNodes);
+        //         String[] nextNodes = parts[1].substring(1, parts[1].length() - 1).split(", ");
+        //         nodes.put(node, nextNodes);
+        //     }
+        //     System.out.println(countSteps("RL", nodes));
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
         for (int i = 0; i < lines.size(); i++) {
             String line = lines.get(i);
             String[] parts = line.split(" = ");
@@ -78,26 +56,42 @@ public class Solution {
             String[] nextNodes = parts[1].substring(1, parts[1].length() - 1).split(", ");
             nodes.put(node, nextNodes);
         }
-
-
+        // System.out.println(countSteps(INS, nodes));
         List<String> startNodes = nodes.keySet().stream()
         .filter(node -> node.endsWith("A"))
         .collect(Collectors.toList());
         Set<String> startSet = new HashSet<>(startNodes);
-        // for (String s : startSet) {
-        //     System.out.println(s);
-        // }
-        int steps = countSteps(startSet, INS, nodes);
-        System.out.println(steps);
+        List<Long> stuffs = new ArrayList<>();
 
-        // for (Map.Entry<String, String[]> node : nodes.entrySet()) {
-        //     if (node.getKey().charAt(2) == ('A')) {
-        //         System.out.println(node.getKey());
-        //     }
-        // }
+        for (String s : startSet) {
+            stuffs.add(countSteps(s, INS, nodes));
+        }
 
+        for (long i : stuffs) {
+            System.out.println(i);
+        }
+
+        System.out.println(bigman(stuffs));
 
         pw.close();
         r.close();
+    }
+
+    public static long lcm(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return lcm(b, a % b);
+    }
+
+    public static long bigman(List<Long> arr) {
+        long lcm = arr.get(0);
+        for (int i = 1; i < arr.size(); i++) {
+            long num1 = lcm;
+            long num2 = arr.get(i);
+            long gcd_val = lcm(num1, num2);
+            lcm = (lcm * arr.get(i)) / gcd_val;
+        }
+        return lcm;
     }
 }
