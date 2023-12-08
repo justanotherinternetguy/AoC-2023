c7e7e899dd929e3f782738c40547f5f68c8802cb
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2023-12-08 02:18:32 -0500

    e

--- Day8/P1/golf.py
+++ Day8/P1/golf.py
@@ -2,7 +2,7 @@ f,i="final","LLRLRRLLRLRRLLRLRRLRRRLRLRLRRRLLRLRRRLRLRRRLRLRLLLRRLRLRLLRLRRLRRRL
 def b(c,z,n):return n[c][1] if z == "R" else n[c][0]
 def _(s,n):
  c,o,g="AAA",0,0
- while c!="ZZZ":c=b(c,"R" if s[g%len(s)]=="R" else "L",n);o+=1;g+=1;print(c,g%len(s))
+ while c!="ZZZ":c=b(c,"R" if s[g%len(s)]=="R" else "L",n);o+=1;g+=1
  return o
 with open(f"{f}.in") as f:k=f.read().splitlines()
 n={l[0]:l[1][1:-1].split(", ") for l in (y.split(" = ") for y in k)}
